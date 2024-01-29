7846db54d98d65d97182be1046af5f0ca459d355
akxlarre <cjentus.benjamin@gmail.com>
2024-01-28 23:26:20 -0300

    Added @ResponseStatus annotations to controller methods

--- src/main/java/proyecto1/demo/Controller/ClienteController.java
+++ src/main/java/proyecto1/demo/Controller/ClienteController.java
@@ -2,6 +2,7 @@ package proyecto1.demo.Controller;
 
 import ch.qos.logback.core.net.server.Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import proyecto1.demo.model.entity.Cliente;
 import proyecto1.demo.service.ICliente;
@@ -14,22 +15,26 @@ public class ClienteController {
     private ICliente clienteService;
 
     @PostMapping("/cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
     @PutMapping("/cliente")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
     @DeleteMapping("/cliente/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id) {
         Cliente clienteDelete = clienteService.findById(id);
         clienteService.delete(clienteDelete);
     }
 
     @GetMapping("/cliente/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Cliente showById(@PathVariable Integer id) {
         return clienteService.findById(id);
     }
